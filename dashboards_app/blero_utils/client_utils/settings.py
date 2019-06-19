4340f567256a954fb6cbb79b74b06b4044dc5634
Eshwanth <jiwa@uber-mail.com>
2019-06-20 01:04:52 +0900

    Updated files for packaging

--- dashboards_app/blero_utils/client_utils/settings.py
+++ dashboards_app/blero_utils/client_utils/settings.py
@@ -1,2 +1,2 @@
-USER_TOKEN="9248001e4a7e1fb4306db1d80aa63cd7ebeb05d7"
-SERVER_ADDRESS="https://blero-server-stage.us.aldryn.io/api/bleroplugin/"
+USER_TOKEN="5bf400fc9f1461429bc9984409f4283ff9fd08fe"
+SERVER_ADDRESS="https://blero.dev/api/bleroplugin/"
